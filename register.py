981a2410e12e1efbe75bbc6bd49fb3080332474a
Rajat Kulkarni <rajatkul1408@gmail.com>
2023-06-07 09:57:22 +0200

    Developed the registration page

--- register.py
+++ register.py
@@ -12,5 +12,37 @@ app = Flask(__name__)
 def home():
     return "Welcome to the registration page!"
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        
+        user_email = request.form["user-email"]
+        password = request.form["password"]
+
+        
+
+        existing_user = collection.find_one({"user-email": user_email})
+        if existing_user:
+            return "Email is already registered!"
+        
+        
+        new_user = {
+            "user-email": user_email,
+            "password": password
+        }
+
+        collection.insert_one(new_user)
+
+        
+        return redirect("/success")
+    else:
+        
+        return render_template("register.html")
+
+@app.route("/success")
+def success():
+    return "Registration successful!"
+
 if __name__ == "__main__":
     app.run(debug=True)

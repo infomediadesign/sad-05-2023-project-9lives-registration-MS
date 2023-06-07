3cb86b8ec1bd59638cdbac9d99a6a5388150228d
Rajat Kulkarni <rajatkul1408@gmail.com>
2023-06-07 09:42:47 +0200

    Created a flask developement server

--- register.py
+++ register.py
@@ -1,7 +1,16 @@
 from pymongo import MongoClient
-
+from flask import Flask, render_template, request, redirect
 
 client = MongoClient("mongodb+srv://rajat011:HxXq4k1ODpXQGjw6@dev.ylhokif.mongodb.net/?retryWrites=true&w=majority")
 #client = MongoClient('localhost', 27017)
 db = client["usersdb"]
 collection = db["login-details"]
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to the registration page!"
+
+if __name__ == "__main__":
+    app.run(debug=True)

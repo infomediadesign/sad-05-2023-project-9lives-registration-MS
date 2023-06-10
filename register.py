bdafb6526fc27a44cf45b29e55e356e5b44785d2
Kish <67649347+Kishor-Patil97@users.noreply.github.com>
2023-06-10 16:47:23 +0200

    fix: registration python and requirement for docker

--- register.py
+++ register.py
@@ -2,13 +2,15 @@ from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, jsonify
 from flasgger import Swagger, swag_from
 
-client = MongoClient("mongodb+srv://rajat011:HxXq4k1ODpXQGjw6@dev.ylhokif.mongodb.net/?retryWrites=true&w=majority")
-#client = MongoClient('localhost', 27017)
+client = MongoClient(
+    "mongodb+srv://rajat011:HxXq4k1ODpXQGjw6@dev.ylhokif.mongodb.net/?retryWrites=true&w=majority")
+# client = MongoClient('localhost', 27017)
 db = client["usersdb"]
 collection = db["login-details"]
 
 app = Flask(__name__)
 swagger = Swagger(app)
+
 
 @app.route("/")
 def home():
@@ -19,17 +21,14 @@ def home():
 @swag_from('swagger/register.yml')
 def register():
     if request.method == "POST":
-        
+
         user_email = request.form["email"]
         password = request.form["password"]
-
-        
 
         existing_user = collection.find_one({"email": user_email})
         if existing_user:
             return "Email is already registered!"
-        
-        
+
         new_user = {
             "email": user_email,
             "password": password
@@ -37,15 +36,16 @@ def register():
 
         collection.insert_one(new_user)
 
-        
         return jsonify({'message': 'Registration successful'}), 201
     else:
-        
+
         return render_template("register.html")
+
 
 @app.route("/success")
 def success():
     return "Registration successful!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
